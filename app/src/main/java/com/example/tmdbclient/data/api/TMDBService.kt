e9b8327255e98aae8e3750ae616cbb10ad4436e9
ArunGohil <arun.r@hungama.com>
2021-09-03 19:05:01 +0530

    Data Layer Part 2

---
+++ app/src/main/java/com/example/tmdbclient/data/api/TMDBService.kt
@@ -0,0 +1,20 @@
+package com.example.tmdbclient.data.api
+
+import com.example.tmdbclient.data.ArtistList
+import com.example.tmdbclient.data.MovieList
+import com.example.tmdbclient.data.TVShowList
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface TMDBService {
+
+    @GET("movie/popular")
+    suspend fun getPopularMovies(@Query("api_key") apiKey: String): Response<MovieList>
+
+    @GET("tv/popular")
+    suspend fun getPopularTvShows(@Query("api_key") apiKey: String): Response<TVShowList>
+
+    @GET("person/popular")
+    suspend fun getPopularArtists(@Query("api_key") apiKey: String): Response<ArtistList>
+}
