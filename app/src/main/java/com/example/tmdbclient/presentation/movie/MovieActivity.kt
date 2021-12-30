4e0c1efbe2644529b0256ce3bd12111ae255c50f
ArunGohil <arun.r@hungama.com>
2021-12-30 19:19:57 +0530

    Presentation Layer Part 4 (Recycler View)

--- app/src/main/java/com/example/tmdbclient/presentation/movie/MovieActivity.kt
+++ app/src/main/java/com/example/tmdbclient/presentation/movie/MovieActivity.kt
@@ -1,19 +1,24 @@
 package com.example.tmdbclient.presentation.movie
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.tmdbclient.R
 import com.example.tmdbclient.databinding.ActivityMovieBinding
 import com.example.tmdbclient.presentation.di.Injector
+import javax.inject.Inject
 
 class MovieActivity : AppCompatActivity() {
+    @Inject
+    lateinit var factory: MovieViewModelFactory
     private lateinit var binding: ActivityMovieBinding
     private lateinit var movieViewModel: MovieViewModel
-    lateinit var factory: MovieViewModelFactory
+    private lateinit var adapter: MovieAdapter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,9 +28,28 @@ class MovieActivity : AppCompatActivity() {
 
         movieViewModel = ViewModelProvider(this, factory)
             .get(MovieViewModel::class.java)
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        binding.movieRecyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = MovieAdapter()
+        binding.movieRecyclerView.adapter = adapter
+        displayPopularMovies()
+    }
+
+    private fun displayPopularMovies() {
+        binding.movieProgressBar.visibility = View.VISIBLE
         val responseLiveData = movieViewModel.getMovies()
-        responseLiveData.observe(this, Observer {
-            Log.i("MYTAG", it.toString())
+        responseLiveData.observe(this, {
+            if (it != null) {
+                adapter.setList(it)
+                adapter.notifyDataSetChanged()
+                binding.movieProgressBar.visibility = View.GONE
+            } else {
+                binding.movieProgressBar.visibility = View.GONE
+                Toast.makeText(applicationContext, "No Data Available", Toast.LENGTH_LONG).show()
+            }
         })
     }
 }
